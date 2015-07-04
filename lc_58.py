3d9a0a22650ee04beba1831c091c73f1fcd048e9
wang101 <hxwang1992@gmail.com>
2015-07-04 22:32:41 +0800

    Length of Last Word

---
+++ lc_58.py
@@ -0,0 +1,22 @@
+class Solution:
+    # @param {string} s
+    # @return {integer}
+    def lengthOfLastWord(self, s):
+        s=s[::-1]
+        num=0
+        tag=0
+        a=[]
+        s=' '+ s + ' '
+        for i in range(len(s)):
+        	if s[i]==' ':
+        		a.append(i)
+        j=0
+        for i in range(len(a)):
+        	if a[i]!=i:
+        		tag=i
+        		break
+        out =  a[tag]-a[tag-1]-1
+        if out>=0:
+            return out
+        else:
+            return 0
